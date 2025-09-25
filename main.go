6f378c1e8bf3838ad1e4a94c0071d7e299975b00
anna khoborova <annhoborova@gmail.com>
2025-09-25 14:56:15 +0300

    новая функция в файле main.go в ветке dev

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ func main() {
 	a := 5
 	b := 7
 	fmt.Println(addNumbers(a, b))
+	fmt.Println(subNumbers(a, b))
 }
 
 func addNumbers(a, b int) int {
 	return a + b
+}
+
+func subNumbers(a, b int) int {
+	return a - b
 }
